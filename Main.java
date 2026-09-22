8e94736699cce626b4c501203348338105ff024f
Javier Casas <javier.casas@iescamas.es>
2026-09-22 10:54:21 +0200

    Modificado desde VS Code

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Saludando con el Nano");
+        System.out.println("Saludando desde VS Code");
     }
 }
